499f7d6bcd00f8aa409b5f705ead9d046c72fa10
Aliaksei Matsarski <alex.matsarski@getzephyr.com>
2018-11-05 21:46:51 -0800

    Add AminController and AdminService

---
+++ src/main/java/com/sjsu/cmpe/sstreet/webserver/controller/AdminController.java
@@ -0,0 +1,8 @@
+package com.sjsu.cmpe.sstreet.webserver.controller;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AdminController {
+}
---
+++ src/main/java/com/sjsu/cmpe/sstreet/webserver/service/AdminService.java
@@ -0,0 +1,7 @@
+package com.sjsu.cmpe.sstreet.webserver.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AdminService {
+}
