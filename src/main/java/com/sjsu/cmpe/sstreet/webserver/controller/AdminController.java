29528b55021c7a0be88c317120780e9177bf1598
aliaksei-matsarski <aliaksei.matsarski@sjsu.edu>
2018-11-06 18:36:50 -0800

    Add ping/pong

--- src/main/java/com/sjsu/cmpe/sstreet/webserver/controller/AdminController.java
+++ src/main/java/com/sjsu/cmpe/sstreet/webserver/controller/AdminController.java
@@ -1,8 +1,16 @@
 package com.sjsu.cmpe.sstreet.webserver.controller;
 
 
+import org.springframework.http.HttpRequest;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AdminController {
+
+    @RequestMapping(path="/ping", method = RequestMethod.GET)
+    public String ping(){
+        return "pong";
+    }
 }
