edb630368c1ba4bc54d7e364796fc9f14cde8d0e
aliaksei-matsarski <aliaksei.matsarski@sjsu.edu>
2018-11-14 20:56:50 -0800

    Add Cassandra repo

--- src/main/java/com/sjsu/cmpe/sstreet/webserver/repository/cassandra/SensorDataCRepository.java
+++ src/main/java/com/sjsu/cmpe/sstreet/webserver/repository/cassandra/SensorDataCRepository.java
@@ -1,10 +1,14 @@
 package com.sjsu.cmpe.sstreet.webserver.repository.cassandra;
 
 import com.sjsu.cmpe.sstreet.webserver.model.SensorData;
+import com.sjsu.cmpe.sstreet.webserver.model.TemperatureSensorData;
 import org.springframework.data.cassandra.repository.CassandraRepository;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SensorDataCRepository extends CassandraRepository<SensorData, String> {
+
+    Slice<SensorData> findAllByIdSmartClusterAndAndIdSensor(Integer idSmartCluster, Integer idSmartNode);
 
 }
--- src/main/java/com/sjsu/cmpe/sstreet/webserver/repository/cassandra/WindDirectionDataRepository.java
+++ src/main/java/com/sjsu/cmpe/sstreet/webserver/repository/cassandra/WindDirectionDataRepository.java
@@ -1,12 +1,13 @@
 package com.sjsu.cmpe.sstreet.webserver.repository.cassandra;
 
 import com.sjsu.cmpe.sstreet.webserver.model.TemperatureSensorData;
+import com.sjsu.cmpe.sstreet.webserver.model.WindDirectionSensorData;
 import org.springframework.data.cassandra.repository.CassandraRepository;
 import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface WindDirectionDataRepository extends CassandraRepository<WindDirectionDataRepository, String> {
+public interface WindDirectionDataRepository extends CassandraRepository<WindDirectionSensorData, String> {
 
-    Slice<WindDirectionDataRepository> findAllByIdSmartClusterAndAndIdSensor(Integer idSmartCluster, Integer idSmartNode);
+    Slice<WindDirectionSensorData> findAllByIdSmartClusterAndAndIdSensor(Integer idSmartCluster, Integer idSmartNode);
 }
