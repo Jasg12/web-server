74b2d8d96abfc310572bd186a6639266c706b09c
Jason G <jjgonsalves7@gmail.com>
2018-12-01 17:56:20 -0800

    Updated ConnectivityController

--- src/main/java/com/sjsu/cmpe/sstreet/webserver/controller/ConnectivityChartController.java
+++ src/main/java/com/sjsu/cmpe/sstreet/webserver/controller/ConnectivityChartController.java
@@ -2,12 +2,14 @@ package com.sjsu.cmpe.sstreet.webserver.controller;
 
 import com.sjsu.cmpe.sstreet.webserver.model.TimeRange;
 import com.sjsu.cmpe.sstreet.webserver.model.statistic.ConnectivityStat;
+import com.sjsu.cmpe.sstreet.webserver.model.statistic.EntityType;
 import com.sjsu.cmpe.sstreet.webserver.service.SearchStatisticsService;
 import com.sjsu.cmpe.sstreet.webserver.service.SmartClusterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -30,10 +32,10 @@ public class ConnectivityChartController {
     @RequestMapping(method = RequestMethod.POST ,value= "/connectivity/for/entitytype/{entity}/id/{id}//from/{from}/to/{to}", produces = "application/json")
     public List<ConnectivityStat> getConnectivityStatByTimeRange(
             @PathVariable(value = "id") Integer id,
-            @PathVariable("entity") EntityType entityType,
+            @RequestParam(name="entityType") EntityType entityType,
             @PathVariable("from") long from,
             @PathVariable("to") long to) {
-        TimeRange timeRange = new TimeRange(from, to);
+            TimeRange timeRange = new TimeRange(from, to);
 
         return searchStatisticsService.getConnectivityStatByTimeRange(id, entityType, timeRange);
     }
